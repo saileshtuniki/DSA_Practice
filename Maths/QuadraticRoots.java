50fcfcc5067ef72cd3c9c58979583e9ef7fbde3f
saileshtuniki <sailesh.tuniki@gmail.com>
2025-06-10 21:48:33 +0530

    Quadratic roots problem

--- Maths/QuadraticRoots.java
+++ Maths/QuadraticRoots.java
@@ -3,18 +3,18 @@ package Maths;
 public class QuadraticRoots {
 
     public static void findRoots (double a, double b, double c){
-        double dirscriminant =  b * b - 4 * a * c;
+        double discriminant =  b * b - 4 * a * c;
         double root1, root2;
 
-        if(dirscriminant > 0){
+        if(discriminant > 0){
             //Real and distinct roots
-            root1 = (-b + Math.sqrt(dirscriminant)/ (2 * a));
-            root2 = (-b - Math.sqrt(dirscriminant)/ (2 * a));
+            root1 = (-b + Math.sqrt(discriminant)/ (2 * a));
+            root2 = (-b - Math.sqrt(discriminant)/ (2 * a));
 
             System.out.println("Real and distinct roots:");
             System.out.println("Root1: " + root1);
             System.out.println("Root2: " + root2);
-        }else if(dirscriminant == 0){
+        }else if(discriminant == 0){
             //real and equal roots
 
             root1 = root2 = -b / (2 * a);
@@ -24,7 +24,7 @@ public class QuadraticRoots {
             //Complex roots
 
             double realPart = -b / (2 * a);
-            double imaginaryPart = Math.sqrt(-dirscriminant) /(2*a);
+            double imaginaryPart = Math.sqrt(-discriminant) /(2*a);
 
             System.out.println("Complex roots:");
             System.out.println("Root1: "+realPart+"+"+imaginaryPart+"i");
